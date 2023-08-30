54887999d6fddb1fa5fb789b0728d3c8df050459
seav1 <107227866+seav1@users.noreply.github.com>
2023-08-30 09:48:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,8 @@ def keepAlive(broswer: Chrome, user, passwd, terminalUrl = ""):
                 break
     print("\rStart KeepAlive Workflow!Enjot it!", end='', flush=True)
     broswer.get(terminalUrl)
-    broswer.implicitly_wait(120)
+    broswer.implicitly_wait(30)
+    sleep(120)
     return 0
 
 def main():
