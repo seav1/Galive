7453b71880f7d51dcc7678dca55571dd50e2b015
seav1 <107227866+seav1@users.noreply.github.com>
2023-08-31 20:54:28 +0800

    Update open-website.js

--- open-website.js
+++ open-website.js
@@ -1,12 +1,19 @@
-const puppeteer = require('puppeteer');
+from selenium import webdriver
+from pyvirtualdisplay import Display
+import time
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://ide-run.goorm.io/workspace/d9Sssb0cUEnisCoRCgX?token=d04702b4519674ab9a512f72b1bae53d&guestname=12');
-  
-  // 延迟1分钟
-  await new Promise(resolve => setTimeout(resolve, 60000));
-  
-  await browser.close();
-})();
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://ide-run.goorm.io/workspace/d9Sssb0cUEnisCoRCgX?token=d04702b4519674ab9a512f72b1bae53d&guestname=12")
+
+# 保持打开状态1分钟
+time.sleep(60)
+
+driver.quit()
+display.stop()
