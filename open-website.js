70f36a7750d078cb9b717735ab7962406ac4e6af
seav1 <107227866+seav1@users.noreply.github.com>
2023-08-31 20:40:21 +0800

    Update open-website.js

--- open-website.js
+++ open-website.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://ide-run.goorm.io/workspace/d9Sssb0cUEnisCoRCgX?token=d04702b4519674ab9a512f72b1bae53d&guestname=12');
   
   // 延迟1分钟
   await new Promise(resolve => setTimeout(resolve, 60000));
