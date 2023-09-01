29c84e845d3b20ae6e1c9d395f71092772085913
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-01 21:05:23 +0800

    Update web_access.py

--- web_access.py
+++ web_access.py
@@ -1,13 +1,15 @@
 import time
-import sched
 from xvfbwrapper import Xvfb
 from seleniumbase import BaseCase
 
 class MyTestClass(BaseCase):
 
+    def setUp(self):
+        super().setUp()
+        self.headless = True  # 设置无头模式
+
     def test_basic(self):
         self.open('https://ide-run.goorm.io/workspace/dGz54pALkYySfIqjBmp?token=ce166f5eccf285db2d818ed4c65a7c12&guestname=12')
-        time.sleep(60)  # 保持1分钟
 
 def run_test():
     with Xvfb() as xvfb:
@@ -17,3 +19,6 @@ def run_test():
             test.test_basic()
         finally:
             test.tearDown()
+
+if __name__ == "__main__":
+    run_test()
