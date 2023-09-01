460953b91c007482ebc5f9a5d389e09c3eca844e
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-01 21:42:40 +0800

    Update web_access.py

--- web_access.py
+++ web_access.py
@@ -5,10 +5,11 @@ class MyTestClass(BaseCase):
     def setUpClass(cls):
         super().setUpClass()
         config.set_headless_browser(False)  # 取消无头模式
+        cls.driver = cls.open_browser()
 
     def test_basic(self):
         self.open('https://ide-run.goorm.io/workspace/dGz54pALkYySfIqjBmp?token=ce166f5eccf285db2d818ed4c65a7c12&guestname=12')
-        time.sleep(60)  # 保持1分钟
+        time.sleep(60)  # 保持1分钟        
 
     def quit(self):
         self.driver.quit()
