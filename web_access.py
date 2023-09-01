c9e056f9645a30e11db7ca22a5b14decb0094e4c
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-01 21:25:34 +0800

    Update web_access.py

--- web_access.py
+++ web_access.py
@@ -1,22 +1,20 @@
-from xvfbwrapper import Xvfb
 from seleniumbase import BaseCase, config
 
 class MyTestClass(BaseCase):
-    def setUp(self):
-        super().setUp()
-        config.set_headless(True)  # 设置无头模式
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        config.set_headless_browser(True)  # 设置无头模式
 
     def test_basic(self):
         self.open('https://ide-run.goorm.io/workspace/dGz54pALkYySfIqjBmp?token=ce166f5eccf285db2d818ed4c65a7c12&guestname=12')
 
 def run_test():
-    with Xvfb() as xvfb:
-        test = MyTestClass()
-        test.setUp()
-        try:
-            test.test_basic()
-        finally:
-            test.tearDown()
+    test = MyTestClass()
+    try:
+        test.test_basic()
+    finally:
+        test.quit()
 
 if __name__ == "__main__":
     run_test()
