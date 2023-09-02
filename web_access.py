f360f60c4ef3375a08aa8fe407b13caccc9ee858
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-02 20:21:59 +0800

    Update web_access.py

--- web_access.py
+++ web_access.py
@@ -1,25 +1,7 @@
-from seleniumbase import BaseCase, config
+from seleniumbase import BaseCase
 
 class MyTestClass(BaseCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        config.set_headless_browser(False)  # 取消无头模式
-        cls.driver = cls.open_browser()
 
-    def test_basic(self):
-        self.open('https://ide-run.goorm.io/workspace/dGz54pALkYySfIqjBmp?token=ce166f5eccf285db2d818ed4c65a7c12&guestname=12')
-        time.sleep(60)  # 保持1分钟        
-
-    def quit(self):
-        self.driver.quit()
-
-def run_test():
-    test = MyTestClass()
-    try:
-        test.test_basic()
-    finally:
-        test.quit()
-
-if __name__ == "__main__":
-    run_test()
+    def test_guest_visit(self):
+        self.open("https://ide-run.goorm.io/workspace/dGz54pALkYySfIqjBmp?token=ce166f5eccf285db2d818ed4c65a7c12&guestname=12")
+        self.sleep(60)
