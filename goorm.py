cd2487f4defa0f99896e5084a5512380f715d6b8
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-07 17:48:53 +0800

    Update goorm.py

--- goorm.py
+++ goorm.py
@@ -1,7 +1,10 @@
-from seleniumbase import BaseCase
+from basecase import BaseCase
 
-class MyTestClass(BaseCase):
+class VisitWebsite(BaseCase):
 
-    def test_guest_visit(self):
-        self.open("https://ide-run.goorm.io/workspace/dGz54pALkYySfIqjBmp?token=ce166f5eccf285db2d818ed4c65a7c12&guestname=12")
-        self.sleep(60)
+    def test_visit_goorm_io(self):
+        self.open('https://ide-run.goorm.io/workspace/dO8D4ys44ocxrovRqIA?token=0312731cf35a27ab2f073e3553c775dc&guestname=13')
+        self.sleep(60)  # Stay on the page for 60 seconds
+
+if __name__ == "__main__":
+    VisitWebsite().set_driver().run()
