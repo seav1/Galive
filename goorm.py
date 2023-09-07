7bbdd74c01e20d3420a764fca59055ca5984677c
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-07 18:08:23 +0800

    Update goorm.py

--- goorm.py
+++ goorm.py
@@ -1,11 +1,19 @@
 from seleniumbase import BaseCase
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 class VisitGoormioPage(BaseCase):
 
-    def test_visit_page(self):
+    def setup_class(self):
         # 设置使用 Tor
-        self.set_proxy_server("127.0.0.1:9050")
-        
+        proxy = Proxy({
+            'proxyType': ProxyType.MANUAL,
+            'httpProxy': '127.0.0.1:9050',
+            'sslProxy': '127.0.0.1:9050',
+            'noProxy': ''
+        })
+        self.driver = self.get_new_driver(proxy=proxy)
+
+    def test_visit_page(self):
         # 访问网址
         self.open("https://ide-run.goorm.io/workspace/dO8D4ys44ocxrovRqIA?token=0312731cf35a27ab2f073e3553c775dc&guestname=13")
         # 停留一分钟
@@ -13,4 +21,5 @@ class VisitGoormioPage(BaseCase):
 
 if __name__ == "__main__":
     case = VisitGoormioPage()
+    case.setup_class()
     case.test_visit_page()
