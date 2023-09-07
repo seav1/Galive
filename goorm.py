c4d4aac8a1e29ceca823eb4c8ad73cefb726c732
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-07 18:23:41 +0800

    Update goorm.py

--- goorm.py
+++ goorm.py
@@ -5,9 +5,15 @@ class VisitGoormioPage(seleniumbase.BaseCase):
     def __init__(self, driver):
         super().__init__()
         self.driver = driver
-        self.set_proxy_server("127.0.0.1:9050")
+
+    def set_proxy_server(self, proxy_url):
+        proxy = webdriver.Proxy()
+        proxy.proxy_url = proxy_url
+        proxy.add_to_capabilities()
+        self.driver.start_session(desired_capabilities=proxy.capabilities)
 
     def test_visit_page(self):
+        self.set_proxy_server("127.0.0.1:9050")
         self.driver.get("https://ide-run.goorm.io/workspace/dO8D4ys44ocxrovRqIA?token=0312731cf35a27ab2f073e3553c775dc&guestname=13")
         self.sleep(60)
 
