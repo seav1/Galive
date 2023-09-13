bf67c46c83cb61ee08ecbc259a637c6939f1aa02
seav1 <107227866+seav1@users.noreply.github.com>
2023-09-13 20:30:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,17 @@
-#!/bin/bash
+# 获取当前时间
+start_time=$(date +%s)
+
+# 打开网址
+xdg-open https://ide-run.goorm.io/workspace/dO8D4ys44ocxrovRqIA?token=0312731cf35a27ab2f073e3553c775dc&guestname=13
+
+# 等待1分钟
+sleep 60
 
 # 获取当前时间
-now=$(date +"%H:%M:%S")
+end_time=$(date +%s)
 
-# 每小时访问一次网址
-while true; do
-    # 检查当前时间是否为整点
-    if [[ "$now" =~ ^[0-9]?:[0-9]?:[00]$ ]]; then
-        # 访问网址
-        open https://ide-run.goorm.io/workspace/dO8D4ys44ocxrovRqIA?token=0312731cf35a27ab2f073e3553c775dc&guestname=13
-        # 保持打开状态 1 分钟
-        sleep 60
-    fi
-    # 获取下一个整点时间
-    next_hour=$(date -d "$now + 1 hour" +"%H:%M:%S")
-    # 等待下一个整点
-    until [[ "$now" = "$next_hour" ]]; do
-        now=$(date +"%H:%M:%S")
-        sleep 1
-    done
-done
+# 计算差值
+duration=$((end_time - start_time))
+
+# 打印差值
+echo "网页保持打开状态${duration}秒"
